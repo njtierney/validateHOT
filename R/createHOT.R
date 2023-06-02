0a270bbe97dc00de372580ea48a733e84c2b0a54
JoshSchramm94 <schramm94@gmail.com>
2023-06-02 11:02:30 +0200

    230602_v01

--- R/createHOT.R
+++ R/createHOT.R
@@ -22,46 +22,92 @@
 #' library(ValiDatHOT)
 #'
 #' # MaxDiff example
-#' data(MaxDiff)
+#' data("MaxDiff")
 #' createHOT(
-#'   data = MaxDiff, None = 19,
-#'   id = 1, prod = 7,
+#'   data = MaxDiff,
+#'   id = 1,
+#'   None = 19,
+#'   prod = 7,
 #'   prod.levels = list(3, 10, 11, 15, 16, 17, 18),
-#'   choice = 20, method = "MaxDiff"
+#'   method = "MaxDiff",
+#'   choice = 20
 #' )
 #'
 #' # CBC example
-#' data(CBC)
+#' data("CBC")
 #' createHOT(
-#'   data = CBC, None = 21,
-#'   id = 1, prod = 3,
+#'   data = CBC,
+#'   id = 1,
+#'   None = 21,
+#'   prod = 3,
 #'   prod.levels = list(c(4, 9, 19), c(8, 12, 17), c(5, 10, 17)),
-#'   choice = 20, method = "CBC", coding = c(0, 0, 0)
+#'   coding = c(0, 0, 0),
+#'   method = "CBC",
+#'   choice = 20
 #' )
 #'
 #' # CBC example with linear coding
-#' data(CBC_lin)
+#' data("CBC_lin")
 #' createHOT(
-#'   data = CBC, None = 21,
-#'   id = 1, prod = 3,
-#'   prod.levels = list(c(4, 9, 19), c(8, 12, 17), c(5, 10, 17)),
-#'   choice = 20, method = "CBC", coding = coding = c(0, 0, 0)
+#'   data = CBC_lin,
+#'   id = 1,
+#'   None = 15,
+#'   prod = 3,
+#'   prod.levels = list(c(4, 9, 60), c(8, 12, 40), c(5, 10, 45)),
+#'   interpolate.levels = list(c(10, 20, 30, 40, 50, 60, 70)),
+#'   lin.p = 14,
+#'   coding = c(0, 0, 1),
+#'   method = "CBC",
+#'   varskeep = 17,
+#'   choice = 16
 #' )
+#'
 #' # ACBC example with linear price
-#' data(ACBC)
+#' data("ACBC")
 #' prod1 <- c(5, 11, 15, 17, 21, 25, 32, 34, 15.99)
 #' prod2 <- c(6, 9, 15, 17, 23, 27, 31, 34, 12.99)
 #' prod3 <- c(8, 12, 16, 19, 23, 24, 28, 34, 12.99)
 #' prod4 <- c(7, 12, 14, 18, 22, 24, 28, 33, 9.99)
 #' prod5 <- c(4, 10, 13, 17, 23, 27, 28, 34, 7.99)
 #' prod6 <- c(5, 9, 14, 17, 23, 27, 29, 33, 9.99)
+#'
 #' createHOT(
-#'   data = ACBC, None = 37,
-#'   id = 1, prod = 6,
+#'   data = ACBC,
+#'   id = 1,
+#'   None = 37,
+#'   prod = 6,
 #'   prod.levels = list(prod1, prod2, prod3, prod4, prod5, prod6),
-#'   choice = 38, method = "ACBC", coding = coding = c(0, 0, 0, 0, 0, 0, 0, 0, 2),
-#'   piece.p = list(c(35,36)), interpolate.levels = c(2.093, 27.287)
+#'   interpolate.levels = list(c(2.093, 27.287)),
+#'   piece.p = list(c(35,36)),
+#'   coding = c(0, 0, 0, 0, 0, 0, 0, 0, 2),
+#'   method = "ACBC",
+#'   choice = 38
 #' )
+#'
+#' data("ACBC_interpolate")
+#' prod1 <- c(5, 5, 12, 14, 18, 22, 29, 31, 15.99)
+#' prod2 <- c(6, 4, 12, 14, 20, 24, 28, 31, 12.99)
+#' prod3 <- c(8, 6, 13, 16, 20, 21, 25, 31, 12.99)
+#' prod4 <- c(7, 5, 11, 15, 19, 21, 25, 30, 9.99)
+#' prod5 <- c(4, 9, 10, 14, 20, 24, 25, 31, 7.99)
+#' prod6 <- c(5, 8, 11, 14, 20, 24, 26, 30, 9.99)
+#'
+#' createHOT(
+#'   data = ACBC_interpolate,
+#'   id = 1,
+#'   None = 39,
+#'   prod = 6,
+#'   prod.levels = list(prod1, prod2, prod3, prod4, prod5, prod6),
+#'   interpolate.levels = list(c(3, 5, 8, 10), c(1.99, 6.99, 9.99, 10.99, 12.99, 17.99, 25.99)),
+#'   piece.p = list(c(36,37), c(35, 36), c(35, 36), c(33,34), c(33, 34), c(33, 34)),
+#'   lin.p = 9,
+#'   coding = c(0, 1, 0, 0, 0, 0, 0, 0, 2),
+#'   method = "ACBC",
+#'   choice = 40
+#'   )
+#'
+#'
+#'
 #'
 #' @export
 createHOT <- function(data, id, None = NULL, prod,
@@ -197,6 +243,11 @@ createHOT <- function(data, id, None = NULL, prod,
         if (coding[pq] == 1) {
           inter.levels <- interpolate.levels[[helper]]
 
+          # error if xout is larger than maximum
+          if (prod.levels[[q]][(pq)] > max(inter.levels)){
+            stop("Error: Extrapolation not possible")
+          }
+
           pos <- lin.p[linear_pos]
 
           lin.levels_eff <- c(scale(inter.levels, center = T, scale = F))
@@ -223,6 +274,11 @@ createHOT <- function(data, id, None = NULL, prod,
           pos.u <- piece.p[[q]][2]
 
           interprice <- prod.levels[[q]][(pq)]
+
+          # error if xout is larger than maximum
+          if (interprice > max(inter.levels)){
+            stop("Error: Extrapolation not possible")
+          }
 
           lower_b <- max(inter.levels[inter.levels < interprice])
           upper_b <- min(inter.levels[inter.levels >= interprice])
