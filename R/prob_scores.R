31a6cd369afe446072a7bd813b17fc6240ba9f6a
JoshSchramm94 <schramm94@gmail.com>
2023-10-16 12:58:27 +0200

    updated prob_scores - 231016

--- R/prob_scores.R
+++ R/prob_scores.R
@@ -8,29 +8,124 @@
 #'
 #' @return a tibble
 #'
+#' @details
+#' \code{prob_scores} converts raw utilities of a MaxDiff to probability scores.
+#'
+#' \code{data} has to be a data frame with the attributes. Items need
+#' to be the raw utilities.
+#'
+#' \code{group} optional grouping variable, if results should be displayed by
+#' different groups. Has to be column name of variables in \code{data}.
+#'
+#' \code{items} specifies the items of the MaxDiff.
+#' Input for \code{items} has to be variable names.
+#'
+#' \code{set.size} specifies the size of the choice sets (how many items were
+#' shown in one task). Input needs to be a whole number.
+#'
+#' \code{anchor} only needs to be specified if anchored MaxDiff is applied. Input
+#' needs to be variable name or column index of anchor variable.
+#'
+#'
+#'
+#' @seealso {
+#' \code{\link[=att_imp]{att_imp}} for attribute importance scores for (A)CBC
+#' }
+#'
+#'
 #' @examples
 #' \dontrun{
 #'
+#' # probability scores for unanchored MaxDiff - without Group defined
+#' prob_scores(data = MaxDiff,
+#'             items = c(Option_01:Option_16),
+#'             set.size = 4
+#')
+#'
+#' # probability scores for unanchored MaxDiff - with Group defined
+#' prob_scores(data = MaxDiff,
+#'             group = Group,
+#'             items = c(Option_01:Option_16),
+#'             set.size = 4
+#')
+#'
+#'
+#' # probability scores for anchored MaxDiff - without Group defined
 #' prob_scores(data = MaxDiff,
 #'             items = c(Option_01:Option_16),
 #'             set.size = 4,
 #'             anchor = "none"
 #')
 #'
+#' # probability scores for anchored MaxDiff - with Group defined
 #' prob_scores(data = MaxDiff,
+#'             group = Group,
 #'             items = c(Option_01:Option_16),
 #'             set.size = 4,
 #'             anchor = "none"
 #')
 #' }
+#'
+#'
 #' @export
 prob_scores <- function(data, group = NULL, items, set.size, anchor = NULL){
 
 
+  if (base::length(data %>% dplyr::select(., {{ items }})) < 2) {
+    base::stop("Error: specify at least 2 items in 'items'!")
+  }
+
+  if (base::anyNA(data %>% dplyr::select(., {{ group }}))) {
+    base::warning("Warning: 'group' contains NAs!")
+  }
+
+  # alternatives
+  ## store names of alternatives
+  alternatives <- data %>%
+    dplyr::select(., {{ items }}) %>%
+    base::colnames()
+
+  ## check whether variable is numeric
+  for (i in 1:base::length(alternatives)) {
+    if (!base::is.numeric(data[[alternatives[i]]])) {
+      base::stop("Error: 'items' has to be numeric!")
+    }
+  }
+
+  ## check for missings
+  if (base::anyNA(data %>% dplyr::select(., {{ items }}))) {
+    base::stop("Error: 'items' contains NAs!")
+  }
+
+  # check set.size
+  if (base::is.null(set.size)){
+    base::stop("Error: 'set.size' is missing!")
+  }
+
+  if (!base::is.numeric(set.size)){
+    base::stop("Error: 'set.size' has to be numeric!")
+  }
+
+  if (set.size > length(data %>% dplyr::select(., {{ items }}))) {
+    base::stop("Error: 'set.size' cannot be larger than number of items!")
+  }
+
+# check anchor
+  if (!(base::is.null(anchor))) {
+    if (!(base::is.numeric(data[anchor]))) {
+      base::stop("Error: 'anchor' needs to be numeric!")
+    }
+  }
+
+  if (!(base::is.null(anchor))) {
+    if (anyNA(data[[anchor]])) {
+      base::stop("Error: 'anchor' contains NAs!")
+    }
+  }
 
 
   if (base::is.null(anchor)){
-    data %>%
+    return(data %>%
       dplyr::mutate(dplyr::across({{ items }}, ~ (base::exp(.x) / (base::exp(.x) + (set.size - 1))))) %>%
       dplyr::rowwise() %>%
       dplyr::mutate(Summe = base::sum(dplyr::pick({{ items }}))) %>% # sum up
@@ -44,7 +139,7 @@ prob_scores <- function(data, group = NULL, items, set.size, anchor = NULL){
       tidyr::pivot_longer(.,
                           cols = tidyselect::ends_with(c(".mw", ".std")),
                           names_to = c("Option", ".value"), names_sep = "\\."
-      )
+      ))
   }
 
   if (!base::is.null(anchor)){
--- tests/testthat/test-prob_scores.R
+++ tests/testthat/test-prob_scores.R
@@ -1,3 +1,72 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("items missing ", {
+  expect_error(prob_scores(data = MaxDiff,
+                           set.size = 4))
 })
+
+test_that("Only 1 item ", {
+  expect_error(prob_scores(data = MaxDiff,
+                           item = Option_01,
+                           set.size = 4))
+})
+
+test_that("Item not numeric ", {
+  data2 <- MaxDiff
+  data2$Option_01 <- as.character(data2$Option_01)
+  expect_error(prob_scores(data = data2,
+                           items = c(Option_01:Option_16),
+                           set.size = 4))
+})
+
+test_that("Group contains NAs - warning ", {
+  data2 <- MaxDiff
+  data2$Group[34] <- NA
+  expect_warning(prob_scores(data = data2,
+                           items = c(Option_01:Option_16),
+                           set.size = 4,
+                           group = Group))
+})
+
+test_that("Items no NAs ", {
+  data2 <- MaxDiff
+  data2$Option_01[34] <- NA
+  expect_error(prob_scores(data = data2,
+                             items = c(Option_01:Option_16),
+                             set.size = 4))
+})
+
+
+test_that("set.size missing ", {
+  expect_error(prob_scores(data = MaxDiff,
+                           items = c(Option_01:Option_16)))
+})
+
+test_that("set.size not numeric ", {
+  expect_error(prob_scores(data = MaxDiff,
+                           items = c(Option_01:Option_16),
+                           set.size = "a"))
+})
+
+test_that("set.size larger than items ", {
+  expect_error(prob_scores(data = MaxDiff,
+                           items = c(Option_01:Option_16),
+                           set.size = 17))
+})
+
+test_that("anchor needs to be numeric ", {
+  data2 <- MaxDiff
+  data2$none <- as.character(data2$none)
+  expect_error(prob_scores(data = data2,
+                           items = c(Option_01:Option_16),
+                           set.size = 4,
+                           anchor = "none"))
+})
+
+test_that("anchor contains NAs ", {
+  data2 <- MaxDiff
+  data2$none[34] <- NA
+  expect_error(prob_scores(data = data2,
+                           items = c(Option_01:Option_16),
+                           set.size = 4,
+                           anchor = "none"))
+})
+
